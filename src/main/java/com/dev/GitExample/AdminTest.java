6a025d17aeee97639edfb01beae5ddd818d295af
poojajadhav0203 <poojajd890@gmail.com>
2024-09-29 13:15:32 +0530

    Admin File Add

---
+++ src/main/java/com/dev/GitExample/AdminTest.java
@@ -0,0 +1,7 @@
+package com.dev.GitExample;
+
+public class AdminTest {
+
+	int i=10;
+
+}
--- src/main/java/com/dev/GitExample/LoginTest.java
+++ src/main/java/com/dev/GitExample/LoginTest.java
@@ -4,6 +4,7 @@ public class LoginTest {
 
 	public static void main(String[] args) {
 		System.out.println("Login File");
+		int i =5;
 
 	}
 
